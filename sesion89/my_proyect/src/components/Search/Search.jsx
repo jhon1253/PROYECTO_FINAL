7b7a6b0f92ec3e33e95f17fb4b5ceec36b218d78
jhon1253 <sebastianquinayas@gmail.com>
2024-07-29 15:53:31 -0500

    pro-f

--- sesion89/my_proyect/src/components/Search/Search.jsx
+++ sesion89/my_proyect/src/components/Search/Search.jsx
@@ -4,6 +4,12 @@ import styles from "./Search.module.css"; // Asegúrate de importar el archivo C
 const SearchBox = ({ searchTerm, onSearchChange }) => {
     return (
       <div className={styles.searchBoxContainer}>
+        <input
+          type="text"
+          placeholder="Search Product"
+          value={searchTerm}
+          onChange={(e) => onSearchChange(e.target.value)}
+        />
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
@@ -14,12 +20,6 @@ const SearchBox = ({ searchTerm, onSearchChange }) => {
         >
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
         </svg>
-        <input
-          type="text"
-          placeholder="Search Product"
-          value={searchTerm}
-          onChange={(e) => onSearchChange(e.target.value)}
-        />
       </div>
     );
 };
