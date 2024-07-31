6db39dac2dabdbb852f12764cec2d350b66f608b
jhon1253 <sebastianquinayas@gmail.com>
2024-07-31 14:36:15 -0500

    pro-f

---
+++ sesion89/my_proyect/src/components/Cart/Cart.jsx
@@ -0,0 +1,20 @@
+import React, { useMemo, useState } from "react";
+import { GiShoppingCart } from "react-icons/gi";
+import styles from "./Cart.module.css";
+import { getCartProducts } from "../../utils/localStorage";
+
+const Cart = ({ productsInCart }) => {
+  return (
+    <button className={styles.button_cart}>
+      <GiShoppingCart size={30} />
+      <p className={styles.products_in_cart}>
+        {productsInCart.reduce(
+          (accumulator, prod) => accumulator + prod.quantity,
+          0
+        )}
+      </p>
+    </button>
+  );
+};
+
+export default Cart
