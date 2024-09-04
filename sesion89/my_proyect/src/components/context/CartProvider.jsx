ca7355392709085bf2c68700406b668f76d5f3b8
jhon1253 <sebastianquinayas@gmail.com>
2024-09-04 15:42:01 -0500

    cambios pro_final

--- sesion89/my_proyect/src/components/context/CartProvider.jsx
+++ sesion89/my_proyect/src/components/context/CartProvider.jsx
@@ -2,20 +2,22 @@ import { useState } from "react";
 import { CartContext } from "./CartContext";
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(
+    JSON.parse(localStorage.getItem("cart")) ?? []
+  );
 
   const addToCart = (product) => {
-    setCart((prevState) => {
-      const existingProdut = prevState.find((item) => item.id === product.id);
-      if (existingProdut) {
-        return prevState.map((item) =>
-          item.id === product.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        );
-      }
-      return [...prevState, { ...product, quantity: 1 }];
-    });
+    let newCart = [];
+    const existingProdut = cart.find((item) => item.id === product.id);
+    if (existingProdut) {
+      newCart = cart.map((item) =>
+        item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+      );
+    } else {
+      newCart = [...cart, { ...product, quantity: 1 }];
+    }
+    localStorage.setItem("cart", JSON.stringify(newCart));
+    setCart(newCart);
   };
 
   const removeFromCart = (product) => {
